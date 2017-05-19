dd9a32ca88ae9561589b8dd4ce4859ae79325c7f
walkews <walker_waylon_s@cat.com>
2017-05-19 16:02:30 -0500

    initial commit

---
+++ app/app.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, render_template, g, jsonify, request
+import pandas as pd
+import settings
+from random import choice
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+
+
+df = pd.read_csv(os.path.join(settings.data_dir, 'pop_by_country_long_form.csv'))
+df['Year'] = df['Year'].str[4:].astype(int)
+df2 = df.groupby(['Year', 'Nation']).sum().unstack()
+df2.columns = df2.columns.droplevel()
+nations = df2.columns
+choices = zip(nations, nations)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+
+class MyForm(FlaskForm):
+    nation = SelectField('nation', choices=choices)
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global nations
+    form = MyForm()
+    return render_template('chart.html', form=form)
+
+
+@app.route('/data/<string:nation>')
+def data(nation):
+    data =  '{' + f'labels:{df2.index.astype(int).tolist()}, series: {df2[nation].astype(float).tolist()}' + '}'
+    return jsonify({'results':df2[nation].astype(float).tolist(), 'labels':df2.index.astype(float).tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ notebooks/settings.py
@@ -0,0 +1,25 @@
+
+import os
+import sys
+from matplotlib import rcParams
+
+rcParams['font.family'] = 'Tunga'
+
+try:
+    root_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+except NameError:
+    root_dir = os.path.abspath(os.path.dirname(_dh[0]))
+except NameError:
+    root_dir = os.path.abspath(os.path.dirname(os.getcwd))
+
+reports_dir = os.path.join(root_dir, 'reports')
+data_dir = os.path.join(root_dir, 'data')
+raw_data_dir = os.path.join(root_dir, 'data', 'raw')
+processed_data_dir = os.path.join(root_dir, 'data', 'processed')
+src_dir = os.path.join(root_dir, 'src')
+analysis_dir = os.path.dirname(root_dir)
+static_dir = os.path.join(src_dir, 'static')
+
+sys.path.append(analysis_dir)
+sys.path.append(root_dir)
+sys.path.append(src_dir)
